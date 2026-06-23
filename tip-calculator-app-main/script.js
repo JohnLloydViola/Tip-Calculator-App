3823c748786681a990c8da1cde76f47120f97610
John Lloyd Viola <145272172+JohnLloydViola@users.noreply.github.com>
2026-06-23 20:17:22 +0800

    Add custom input error handling; display error messages for invalid input

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -2,7 +2,6 @@ let buttonVal = document.querySelectorAll('.btnVal');
 let inputVal = document.getElementById('custom-btn');
 let resetBtn = document.querySelector('.reset-btn');
 
-
 let tipAmount = undefined;
 let totalAmount = undefined;
 resetBtn.disabled = true;
@@ -11,12 +10,36 @@ inputVal.addEventListener('click', () => {
   removeActiveBtn(buttonVal);
 });
 
+inputVal.addEventListener("input", () => { 
+  let billInputStr = document.getElementById('bill-input').value.trim();
+  let numberPeopleInStr = document.getElementById('people-input').value.trim();
+  let input = Number(document.getElementById('custom-btn').value.trim());
+  let customErrorMsg = document.getElementById('custom-error-msg');
+
+  if (isNaN(input)) {
+    document.getElementById('custom-error-msg').innerText = 'Numbers Only';
+    document.getElementById('custom-error-msg').style.color = 'red';
+    return;
+  }else if(input < 0) {
+    document.getElementById('custom-error-msg').innerText = "Can't be negative ";
+    document.getElementById('custom-error-msg').style.color = 'red';
+    return;
+  }
+  if(!isNaN(input)) {
+    document.getElementById('custom-error-msg').innerText = ''
+  }
+  inputValidation(billInputStr, numberPeopleInStr, Number(input));
+});
+
+
 resetBtn.addEventListener('click', () => {
   document.getElementById('bill-input').value = "";
   document.getElementById('people-input').value = "";
   document.getElementById('custom-btn').value = "";
   document.getElementById('tip-value').innerHTML = "&#36;0.00";
   document.getElementById('total-value').innerHTML = "&#36;0.00";
+  document.getElementById('bill-error-msg').innerText ='';
+  document.getElementById('number-error-msg').innerText ='';
   removeActiveBtn(buttonVal);
   resetBtn.classList.remove('reset-active');
   resetBtn.disabled = true;
@@ -30,14 +53,16 @@ function getBtnValues(button) {
     buttons.addEventListener('click', () =>{
       removeActiveBtn(button);
       buttons.classList.add('activeBtn');
+      document.getElementById('custom-error-msg').innerText = '';
+      document.getElementById('custom-btn').value = '';
+      
 
       let buttonVal = buttons.getAttribute('data-percentage-value');
-      let billInputStr = document.getElementById('bill-input').value.trim();
-      let numberPeopleInStr = document.getElementById('people-input').value.trim();
-       console.log(billInputStr);
+      let billInputStr = Number(document.getElementById('bill-input').value.trim());
+      let numberPeopleInStr = Number(document.getElementById('people-input').value.trim());
+
       inputValidation(billInputStr, numberPeopleInStr, buttonVal);
     });
-    
   });
 }
 
@@ -49,6 +74,11 @@ function inputValidation(billInput, numberInput, buttonVal) {
     billErrorMsg.innerText = "Can't be zero";
     billErrorMsg.style.color = 'red';
     billErrorMsg.style.display = 'block';
+    return;
+  }else if (billInput < 0) {
+    billErrorMsg.innerText = "Can't be negative";
+    billErrorMsg.style.color = 'red';
+    billErrorMsg.style.display = 'block'
     return;
   }else if (isNaN(billInput)) {
     billErrorMsg.innerText = "Numbers Only";
@@ -64,6 +94,11 @@ function inputValidation(billInput, numberInput, buttonVal) {
     numberErrorMsg.style.color = 'red';
     numberErrorMsg.style.display = 'block';
     return;
+  }else if(numberInput < 0) {
+    numberErrorMsg.innerText = "Can't be negative";
+    numberErrorMsg.style.color = 'red';
+    numberErrorMsg.style.display = 'block'
+
   }else if (isNaN(numberInput)) {
     numberErrorMsg.innerText = "Numbers Only";
     numberErrorMsg.style.color = 'red';
